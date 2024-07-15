0cdf7208510d5b86e8f0f0733094758f4fce511b
Shanon Palomino Salinas <ssalinas@cfpboard.org>
2024-07-15 19:12:37 -0400

    Create folder and files for models, controllers, and connections for mongoose database use

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
+/**
+ * Import Dependencies
+ */
+
 require("dotenv").config(); // Load ENV Variables
-const express = require("express"); // imPORT express
-const morgan = require("morgan"); //imPORT morgan
+const express = require("express");
+const path = require('path');
+const favicon = require('serve-favicon');
+const morgan = require("morgan"); 
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
-console.log(PORT, MONGO_URI)
+
 
 const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+/**
+ * MIDDLEWARE 
+ * serve-favicon & static middleware
+ * to serve from the production 'build' folder*/
+ 
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
 /*
-* Database Connection
+* DATABASE CONNECTION
 */
 
 const CONFIG = {
@@ -26,11 +44,20 @@ mongoose.connection
   .on("close", () => console.log("Disconnected from Mongoose"))
   .on("error", (error) => console.log(error));
 
+/**
+ * ROUTES
+ */
 
 app.get("/", (req, res) => {
     res.send("Hello world!");
 });
 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html' ));
+});
+/**
+ * SERVER LISTENER 
+ */
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
 })
